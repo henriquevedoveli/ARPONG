0c5b44723bea64f5a298db1165612bef1e8eeb3d
henriquevedoveli <henriquevedoveli@gmail.com>
2023-07-22 20:25:02 -0300

    refactor

--- main.py
+++ main.py
@@ -7,95 +7,137 @@ import handTracking
 BRUSH_THICKNESS = 15
 ERASE_THICKNESS = 25
 FOLDER_PATH = 'assets'
-DRAW_COLOR = (0, 255, 0)  # Green color
-XP, YP = 0, 0
+DRAW_COLOR = (0, 255, 0)  
 WCAM, HCAM = 640, 480
 DETECTION_CON = 0.8
 
-# Load color images for the header
-myList = os.listdir(FOLDER_PATH)
-overlayList = [cv2.imread(f'{FOLDER_PATH}/{imgPath}') for imgPath in myList]
-header = overlayList[0]
+def load_overlay_images(folder_path):
+    """
+    Load overlay images for the header.
 
-# Initialize the camera
-cap = cv2.VideoCapture(0)
-cap.set(3, WCAM)
-cap.set(4, HCAM)
+    Parameters:
+    folder_path (str): The path to the folder containing the images.
 
-detector = handTracking.Detector(detectionCon=DETECTION_CON)
+    Returns:
+    list: A list of overlay images.
+    """
+    my_list = os.listdir(folder_path)
+    overlay_list = [cv2.imread(f'{folder_path}/{img_path}') for img_path in my_list]
+    return overlay_list
 
-# Create an image to store the drawing
-imgCanvas = np.zeros((HCAM, WCAM, 3), np.uint8)
+def create_image_for_drawing(height, width):
+    """
+    Create an image to store the drawing.
 
-while True:
-    # Capture the frame
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
+    Parameters:
+    height (int): The height of the image.
+    width (int): The width of the image.
 
-    # Find hand landmarks
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
+    Returns:
+    numpy.ndarray: The image for drawing.
+    """
+    img_canvas = np.zeros((height, width, 3), np.uint8)
+    return img_canvas
 
-    if len(lmList) != 0:
-        # Get the coordinates of the middle and index fingers
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
+def select_color(x, y, header, draw_color):
+    """
+    Select the drawing color based on the coordinates.
 
-        # Check which fingers are up
-        fingers = detector.fingersUp()
+    Parameters:
+    x (int): The x-coordinate of the selected point.
+    y (int): The y-coordinate of the selected point.
+    header (numpy.ndarray): The header image.
+    draw_color (tuple): The current drawing color.
 
-        # Selection Mode: Two fingers are up
-        if fingers[1] and fingers[2]:
-            XP, YP = 0, 0
-            print('Selection Mode')
-            if y1 < 90:
-                # Color selection based on x-coordinate
-                if 250 < x1 < 350:
-                    header = overlayList[2]
-                    DRAW_COLOR = (255, 0, 0)
-                elif 500 < x1 < 600:
-                    header = overlayList[1]
-                    DRAW_COLOR = (0, 0, 0)
-                elif 400 < x1 < 450:
-                    header = overlayList[0]
-                    DRAW_COLOR = (0, 255, 0)
-                elif 70 < x1 < 150:
-                    header = overlayList[3]
-                    # Blue with reduced brightness
-                    DRAW_COLOR = (0, 0, 255)
+    Returns:
+    tuple: The updated header image and drawing color.
+    """
+    if y < 90:
+        if 250 < x < 350:
+            header = overlay_list[2]
+            draw_color = (255, 0, 0)  # Red color
+        elif 500 < x < 600:
+            header = overlay_list[1]
+            draw_color = (0, 0, 0)  # Black color
+        elif 400 < x < 450:
+            header = overlay_list[0]
+            draw_color = (0, 255, 0)  # Green color
+        elif 70 < x < 150:
+            header = overlay_list[3]
+            draw_color = (0, 0, 255)  # Blue color with reduced brightness
+    return header, draw_color
 
-            cv2.rectangle(img, (x1, y1-20), (x2, y2+20), DRAW_COLOR, cv2.FILLED)
+def main():
+    # Load color images for the header
+    overlay_list = load_overlay_images(FOLDER_PATH)
+    header = overlay_list[0]
 
-        # Drawing Mode: Index finger is up
-        if fingers[1] and not fingers[2]:
-            cv2.circle(img, (x1, y1), 15, DRAW_COLOR, cv2.FILLED)
-            print('Drawing Mode')
+    # Initialize the camera
+    cap = cv2.VideoCapture(0)
+    cap.set(3, WCAM)
+    cap.set(4, HCAM)
 
-            if XP == 0 and YP == 0:
+    detector = handTracking.Detector(detectionCon=DETECTION_CON)
+
+    # Create an image to store the drawing
+    img_canvas = create_image_for_drawing(HCAM, WCAM)
+
+    while True:
+        # Capture the frame
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+
+        # Find hand landmarks
+        img = detector.findHands(img)
+        lm_list = detector.findPosition(img, draw=False)
+
+        if len(lm_list) != 0:
+            # Get the coordinates of the middle and index fingers
+            x1, y1 = lm_list[8][1:]
+            x2, y2 = lm_list[12][1:]
+
+            # Check which fingers are up
+            fingers = detector.fingersUp()
+
+            # Selection Mode: Two fingers are up
+            if fingers[1] and fingers[2]:
+                print('Selection Mode')
+                header, DRAW_COLOR = select_color(x1, y1, header, DRAW_COLOR)
+                cv2.rectangle(img, (x1, y1-20), (x2, y2+20), DRAW_COLOR, cv2.FILLED)
+
+            # Drawing Mode: Index finger is up
+            if fingers[1] and not fingers[2]:
+                print('Drawing Mode')
+                cv2.circle(img, (x1, y1), 15, DRAW_COLOR, cv2.FILLED)
+
+                if XP == 0 and YP == 0:
+                    XP, YP = x1, y1
+
+                thickness = ERASE_THICKNESS if DRAW_COLOR == (0, 0, 0) else BRUSH_THICKNESS
+                cv2.line(img, (XP, YP), (x1, y1), DRAW_COLOR, thickness)
+                cv2.line(img_canvas, (XP, YP), (x1, y1), DRAW_COLOR, thickness)
+
                 XP, YP = x1, y1
 
-            if DRAW_COLOR == (0, 0, 0):
-                cv2.line(img, (XP, YP), (x1, y1), DRAW_COLOR, ERASE_THICKNESS)
-                cv2.line(imgCanvas, (XP, YP), (x1, y1), DRAW_COLOR, ERASE_THICKNESS)
-            else:
-                cv2.line(img, (XP, YP), (x1, y1), DRAW_COLOR, BRUSH_THICKNESS)
-                cv2.line(imgCanvas, (XP, YP), (x1, y1), DRAW_COLOR, BRUSH_THICKNESS)
+        # Convert the drawing to grayscale and apply threshold
+        img_gray = cv2.cvtColor(img_canvas, cv2.COLOR_BGR2GRAY)
+        _, img_inv = cv2.threshold(img_gray, 50, 255, cv2.THRESH_BINARY_INV)
+        img_inv = cv2.cvtColor(img_inv, cv2.COLOR_GRAY2BGR)
 
-            XP, YP = x1, y1
+        # Combine the webcam image with the drawing
+        img = cv2.bitwise_and(img, img_inv)
+        img = cv2.bitwise_or(img, img_canvas)
 
-    # Convert the drawing to grayscale and apply threshold
-    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
-    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
-    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+        img[0:90, 0:640] = header
+        cv2.imshow('Image', img)
 
-    # Combine the webcam image with the drawing
-    img = cv2.bitwise_and(img, imgInv)
-    img = cv2.bitwise_or(img, imgCanvas)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    img[0:90, 0:640] = header
-    cv2.imshow('Image', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cap.release()
+    cv2.destroyAllWindows()
 
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    XP, YP = 0, 0  # Initialize the XP and YP variables
+    main()
+
