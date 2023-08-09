12bebccdf43f0825a431b9dd160492be866f9bbb
henriquevedoveli <henriquevedoveli@gmail.com>
2023-08-09 20:41:08 -0300

    implementacao jogo

---
+++ game.py
@@ -0,0 +1,157 @@
+import cv2 as cv
+import numpy as np
+from typing import List
+from handDetection import HandDetector
+import utils
+
+class Game:
+    def __init__(self) -> None:
+        """
+        Initialize the Game class.
+
+        This constructor initializes the video capture, hand detector, game assets,
+        and game state variables.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        self.cap: cv.VideoCapture = cv.VideoCapture(2)
+        self.hd: HandDetector = HandDetector(detectionCon=0.7)
+
+        self.width: int = 640
+        self.height: int = 480
+        self.cap.set(cv.CAP_PROP_FRAME_WIDTH, self.width)
+        self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.height)
+
+        self.load_assets()
+        self.initialize_game_state()
+
+    def load_assets(self) -> None:
+        """
+        Load game assets.
+
+        Load and resize game images (ball, plataform, game over screen).
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        self.ball: np.ndarray = cv.resize(cv.imread('assets/ball.png', cv.IMREAD_UNCHANGED), (40, 40))
+        self.plataform: np.ndarray = cv.resize(cv.imread('assets/plataform.png', cv.IMREAD_UNCHANGED), (120, 26))
+        self.gameover: np.ndarray = cv.resize(cv.imread('assets/gameover.png', cv.IMREAD_UNCHANGED), (self.width, self.height))
+
+    def initialize_game_state(self) -> None:
+        """
+        Initialize game state variables.
+
+        Set the initial values for position, speed, and score.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        self.position: List[int] = [50, 50]
+        self.speedx: int = 15
+        self.speedy: int = 15
+        self.score: int = 0
+
+    def process_hands(self, hands: List[dict]) -> None:
+        """
+        Process hand gestures.
+
+        Analyze hand gestures to determine plataform movement and update the game state accordingly.
+
+        Args:
+            hands (List[dict]): List of hand information dictionaries
+
+        Returns:
+            None
+        """
+        if hands:
+            bbox: dict = hands[0]['bbox']
+            x, y, w, h = bbox
+            h1, w1, c = self.plataform.shape
+            x1: int = x - h1 // 2
+            x1 = np.clip(x1, 5, 510)
+            self.image = utils.overlayPNG(self.image, self.plataform, [x1, 447])
+
+            if self.position[1] < 50:
+                self.speedy = -self.speedy
+                self.position[0] -= 10
+
+            if x1 - 10 < self.position[0] < x1 + w1 and 380 < self.position[1] < 380 + h1:
+                self.speedy = -self.speedy
+                self.position[0] += 30
+                self.score += 1
+
+    def update_position(self) -> None:
+        """
+        Update ball position and check game over condition.
+
+        Update ball position based on speed and direction, and handle game over condition.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        if self.position[1] > 400:
+            self.image = self.gameover
+            # Display game over message and instructions
+            utils.putTextRect(self.gameover, f'Final score {self.score}', [300, 190], 1.9, 2, colorR=(0, 0, 0))
+            utils.putTextRect(self.gameover, 'Press R to restart', [290, 305], 1.82, 2, colorR=(0, 0, 0))
+            utils.putTextRect(self.gameover, 'Press Esc to quit', [290, 345], 1.82, 2, colorR=(0, 0, 0))
+        else:
+            if self.position[0] >= 560 or self.position[0] <= 20:
+                self.speedx = -self.speedx
+            self.position[0] += self.speedx
+            self.position[1] += self.speedy
+
+    def run(self) -> None:
+        """
+        Main game loop.
+
+        Run the game loop that processes user input, updates game state, and displays the game frame.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        while True:
+            ret, self.image = self.cap.read()
+            self.image = cv.flip(self.image, 1)
+
+            cv.rectangle(self.image, (0, self.height), (self.width, 0), (0, 0, 255), 8)
+            cv.rectangle(self.image, (0, self.height - 40), (self.width, self.height), (0, 255, 255), 6)
+
+            hands, self.image = self.hd.findHands(self.image, flipType=True)
+            self.process_hands(hands)
+            self.update_position()
+
+            utils.putTextRect(self.image, f'Score {self.score}', [270, 30], 1.5)
+            try:
+                self.image = utils.overlayPNG(self.image, self.ball, self.position)
+                cv.imshow('frame', self.image)
+                key = cv.waitKey(1)
+            except:
+                break
+
+            if key in [ord('r'), ord('R')]:
+                self.initialize_game_state()  # Reset game state
+                self.load_assets()            # Reload game assets
+            elif key == 27:  # 27 is the ASCII code for the 'Esc' key
+                break
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
---
+++ utils.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+
+
+def overlayPNG(imgBack, imgFront, pos=[0, 0]):
+    hf, wf, cf = imgFront.shape
+    hb, wb, cb = imgBack.shape
+    *_, mask = cv2.split(imgFront)
+    maskBGRA = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+    maskBGR = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    imgRGBA = cv2.bitwise_and(imgFront, maskBGRA)
+    imgRGB = cv2.cvtColor(imgRGBA, cv2.COLOR_BGRA2BGR)
+
+    imgMaskFull = np.zeros((hb, wb, cb), np.uint8)
+
+    imgMaskFull[pos[1]:hf + pos[1], pos[0]:wf + pos[0], :] = imgRGB
+
+    imgMaskFull2 = np.ones((hb, wb, cb), np.uint8) * 255
+    maskBGRInv = cv2.bitwise_not(maskBGR)
+    imgMaskFull2[pos[1]:hf + pos[1], pos[0]:wf + pos[0], :] = maskBGRInv
+
+    imgBack = cv2.bitwise_and(imgBack, imgMaskFull2)
+    imgBack = cv2.bitwise_or(imgBack, imgMaskFull)
+
+    return imgBack
+
+
+
+def putTextRect(
+    img:np.array,
+    text:str,
+    pos:list,
+    scale=3,
+    thickness=3,
+    colorT=(255, 255, 255),
+    colorR=(255, 0, 255),
+    font=cv2.FONT_HERSHEY_PLAIN,
+    offset=10,
+    border=None,
+    colorB=(0, 255, 0),
+):
+    ox, oy = pos
+    (w, h), _ = cv2.getTextSize(text, font, scale, thickness)
+
+    x1, y1, x2, y2 = ox - offset, oy + offset, ox + w + offset, oy - h - offset
+
+    cv2.rectangle(img, (x1, y1), (x2, y2), colorR, cv2.FILLED)
+    if border is not None:
+        cv2.rectangle(img, (x1, y1), (x2, y2), colorB, border)
+    cv2.putText(img, text, (ox, oy), font, scale, colorT, thickness)
+
+    return img, [x1, y2, x2, y1]
